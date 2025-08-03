59c355c725c3d7b3da9ca43ef7dcf0738e66e10d
baobeinet521 <zhengdan_nacy@163.com>
2025-08-03 18:16:26 +0800

    message多进程通信

---
+++ app/src/main/java/com/zd/study/service/MessengerService.java
@@ -0,0 +1,52 @@
+package com.zd.study.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MessengerService extends Service {
+    private static final String TAG = "MessengerService";
+    // 将Messenger和Handler关联起来
+    private Messenger mServiceMessenger = new Messenger(new MessengerHandler());
+
+    public MessengerService() {
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mServiceMessenger.getBinder();
+    }
+
+    private static class MessengerHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MessengerActivity.MESSAGE_FROM_CLIENT:
+                    // 打印接收到的客户端消息
+                    Log.e(TAG, "receive message from client:" + msg.getData().getString("msg"));
+                    // 给客户端回复一条消息
+                    Messenger clientMessenger = msg.replyTo;
+                    Message message = Message.obtain();
+                    message.what = MessengerActivity.MESSAGE_FROM_SERVICE;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("msg", "I am fine,thank you!");
+                    message.setData(bundle);
+                    try {
+                        clientMessenger.send(message);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    }
+}
